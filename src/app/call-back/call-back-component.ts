a7ad27b012fd92da7f4a476253180cbbb693ace2
Jonathan Martin <jomartin@udel.edu>
2018-12-04 21:54:15 -0500

    Now login works

--- src/app/call-back/call-back-component.ts
+++ src/app/call-back/call-back-component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-call-back-component',
-  templateUrl: './call-back-component.component.html',
-  styleUrls: ['./call-back-component.component.scss']
+  templateUrl: './call-back-component.html',
+  styleUrls: ['./call-back-component.scss']
 })
 export class CallBackComponent implements OnInit {
 
